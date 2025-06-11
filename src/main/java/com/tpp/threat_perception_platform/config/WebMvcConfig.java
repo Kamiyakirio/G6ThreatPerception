9fcde451cc6ebb87f571812c31363a3248d68f06
sjy722 <1362690893@qq.com>
2025-06-11 14:02:44 +0800

    Update WebMvcConfig.java

--- src/main/java/com/tpp/threat_perception_platform/config/WebMvcConfig.java
+++ src/main/java/com/tpp/threat_perception_platform/config/WebMvcConfig.java
@@ -28,5 +28,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         //host related pages
         registry.addViewController("/page/host/list").setViewName("host/list");
         registry.addViewController("/page/host/detect").setViewName("host/detect");
+        registry.addViewController("/page/host/result_display").setViewName("host/result_display");
+
     }
 }
