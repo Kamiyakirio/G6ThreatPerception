c07f92c4af523b5f0237d341dd783fa97f9b8ea4
KamiyaKirio <cindy@SHIVA>
2025-06-18 14:46:21 +0800

    全面接入AI，同时修改部分已有逻辑

--- src/main/java/com/tpp/threat_perception_platform/controller/RabbitMQController.java
+++ src/main/java/com/tpp/threat_perception_platform/controller/RabbitMQController.java
@@ -204,7 +204,7 @@ public class RabbitMQController {
                         service.setExtrainfo(serviceData.getString("extrainfo"));
 
 
-                        serviceMapper.updateByPrimaryKeySelective(service);
+                        serviceMapper.insertSelective(service);
                     }
                 }
             }
