7ae13e7066e75934ade2a2b638f894ab3ad3b0f0
hhhyyyxxxx <145080646+hhhyyyxxxx@users.noreply.github.com>
2025-06-19 11:16:29 +0800

    Update AIServiceImpl.java

--- src/main/java/com/tpp/threat_perception_platform/service/impl/AIServiceImpl.java
+++ src/main/java/com/tpp/threat_perception_platform/service/impl/AIServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 
 @Service
 public class AIServiceImpl implements AIService {
-    private final String ApiKey="sk-fbqkipctednqaduaxghzxwleetvbnxqoegzijtrmtuxcqmpy";
+    private final String ApiKey="sk-msqkwaaazrumwgtqqlolfkeupojnbherwlvxzsrwpabuaoep";
     private JSONObject defaultSettings=new JSONObject();
 
     public AIServiceImpl(){
