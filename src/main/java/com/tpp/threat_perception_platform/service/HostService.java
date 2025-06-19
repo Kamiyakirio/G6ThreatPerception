221be2f64cf0d31e16cb3e6476b2b7486cd8284e
sjy722 <1362690893@qq.com>
2025-06-19 09:58:18 +0800

    新增host/list.html中显示已探测主机数和在线主机数的卡片

--- src/main/java/com/tpp/threat_perception_platform/service/HostService.java
+++ src/main/java/com/tpp/threat_perception_platform/service/HostService.java
@@ -20,6 +20,12 @@ public interface HostService {
     
     public Host selectByMacAddress(String macAddress);
 
+    /**
+     * 获取主机统计数据（已探测/在线数量）
+     * @return 包含统计数据的HashMap
+     */
+    public HashMap<String, Integer> getHostStatistics();
+
     public HashMap<String,Object> hostDetect(HashMap<String,Object> data);
 
 
